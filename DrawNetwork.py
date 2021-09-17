d450a9b0e82a0fb35c54b7b154cab3389aca45c0
shahbaz22 <5h4h842.c@gmail.com>
2021-09-17 18:21:22 +0100

    added animation

--- DrawNetwork.py
+++ DrawNetwork.py
@@ -2,165 +2,137 @@ import numpy as np
 import numpy as np
 import pandas as pd
 import networkx as nx
-# import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
-from IPython.display import Image
-from mpl_toolkits.basemap import Basemap as Basemap
-import matplotlib.cbook
+import cartopy.crs as ccrs
+from cartopy.feature.nightshade import Nightshade
+import cartopy.feature as cfeature
 from datetime import datetime
 from collections import Counter
-
-
-# first want to draw network for single specific time, eventually want to run this plotting class a bunch of times (class usful to acces attributes and run methods)
+from celluloid import Camera
+# rework with cartopy
+# first want to draw network for single specific time, eventually want to run this 
+# plotting class a bunch of times (class usful to acces attributes and run methods)
 # Need to creat a dictonary which maps station name to (key)conda evs
- 
-
-
 
 
 class DrawNetwork:
-    def __init__(self, edge_list_dir: str, n: int, directed: bool):
+    def __init__(self, edge_list_dir: str, n: int, directed: bool, geomag_poles_coord:dict):
         G = nx.read_edgelist(edge_list_dir)
-        self.edge_list = sorted( list(G.edges(data=True)), key = lambda e: e[2]['attr_dict']['UTC1'])
-        self.all_times = [t[2]['attr_dict']['UTC1'] for t in self.edge_list]
+        self.edge_list = list(G.edges(data=True))
+        all_times = [s[2]['attr_dict']['UTC1'] for s in self.edge_list]
+        self.ordered_times = sorted(list(set(all_times)))
         # print(times_check)
         self.n = n
         self.directed = directed
         self.station_data = pd.read_csv('supermag-stations.csv')
         self.station_data.set_index('IAGA', inplace=True)
+        self.geomag_poles_coord = geomag_poles_coord
+        self.date = self.edge_list[0][2]['attr_dict']['UTC1'].split(' ')[0]
 
 
+    def t_close(self, target_time: str) -> str:
+        date_time = f'{self.date} {target_time}'
+        date_format = '%Y-%m-%d %H:%M:%S'
+        self.closest_time = min(self.all_times, key=lambda x: 
+            abs(datetime.strptime(x, date_format) - datetime.strptime(date_time, date_format)))
+        self.datetime_closest_time = datetime.strptime(self.closest_time,'%Y-%m-%d %H:%M:%S')
+        return self.closest_time
 
-    def t_snapshot(self, target_time: str) -> None:
-        date = self.edge_list[0][2]['attr_dict']['UTC1'].split(' ')[0]
-        date_time = f'{date} {target_time}'
-        # print(self.all_times)
-        date_f = '%Y-%m-%d %H:%M:%S'
-        closest_time = min(self.all_times, key=lambda x: 
-            abs(datetime.strptime(x, date_f) - datetime.strptime(date_time, date_f)))
-        # print(closest_time, type(closest_time))       
+    def cartopy_ax(self, fig: plt.figure, date_time: str, proj: str, G:nx.Graph, available_stations:pd.DataFrame, counts:dict, label:str) ->None:
+
+        year = self.date.split('-')[0]
+        gn_lat, gn_lon  = self.geomag_poles_coord[int(year)]['north']
+        gs_lat, gs_lon = self.geomag_poles_coord[int(year)]['south']
+        source_proj = ccrs.PlateCarree() 
+        map_proj = ccrs.EckertII(central_longitude=gn_lon)
+        ax = plt.axes(projection = map_proj)
+        ax.stock_img()
+        ax.add_feature(cfeature.GSHHSFeature(edgecolor='k'))          
+        node_size_factor = 0.01
+        dt = datetime.strptime(date_time,'%Y-%m-%d %H:%M:%S')
+        ax.add_feature(Nightshade(dt, alpha=0.2))
+        ax.scatter(x=[gn_lon,gs_lon], y=[gn_lat,gs_lat], color="orange", s=100,alpha=1, transform=source_proj)
+        title = ax.text(0.5,1.05, "", bbox={'facecolor':'w', 'alpha':0.5, 'pad':5}, transform=ax.transAxes, ha="center")
+        title.set_text(f'{label}\n{date_time} UTC')
+        pos = {}
+        pos = {station: (map_proj.transform_point(v['GEOLON'], v['GEOLAT'], src_crs=source_proj))
+           for station, v in
+           available_stations.to_dict('index').items()}
+        freqs = [node_size_factor*counts[station]**2.8 for station in G.nodes()]
+        nx.draw_networkx_nodes(G = G, pos = pos, nodelist = G.nodes(), 
+        node_color = 'r', alpha = 0.8, node_size = freqs, ax = ax)
+        nx.draw_networkx_edges(G = G, pos = pos, edge_color='g',
+        alpha=1, arrows = self.directed, ax = ax, nodelist = G.nodes(),)
+
+    def t_snapshot(self, target_time: str, fig:plt.figure, label:str) -> None:
+        
         t_edge_list = []
-        # need to find time closest to, due to only having time windwows
+        # # need to find time closest to, due to only having time windwows
         for line in self.edge_list:
             # can use indexer if needing multiple times
-            if line[2]['attr_dict']['UTC1'] == date_time:
+            if line[2]['attr_dict']['UTC1'] == target_time:
                 t_edge_list.append([line[0].split('_')[0], line[1].split('_')[0]])
 
         if self.directed == True:
             G = nx.DiGraph(t_edge_list)
         else:
-            G = nx.Graph(t_edge_list) 
+            G = nx.Graph(t_edge_list)
 
         reshaped_edge_list = np.reshape(t_edge_list, len(t_edge_list)*2)
-
         stations = set(reshaped_edge_list)
-
-        print('stations', stations)
-        print('stations method', G.nodes())
-
         counts = Counter(reshaped_edge_list)
-
-        print(self.station_data.head())
-
-        available_stations = self.station_data[self.station_data.index.isin(stations)]
-
-        # print('target_time',target_time)
-        # print('closest_time',closest_time)
-        # print(t_edge_list)
-
-        # llcrnrlat,llcrnrlon,urcrnrlat,urcrnrlon
-        # are the lat/lon values of the lower left and upper right corners
-        # of the map.
-        # lat_ts is the latitude of true scale.
-        # resolution = 'c' means use crude resolution coastlines.
-        # m = Basemap(projection='merc' ,llcrnrlat=-85, urcrnrlat=85,
-        #             llcrnrlon=-180, urcrnrlon=180, resolution='c')
-
-        # maybe do a global network and a section of small pannels showing greater detail.
-
-        fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(15,15))
-
-        lon_0 = -105; lat_0 = 40
-        m1 = Basemap(ax = ax[0,0], projection='aeqd', lat_0=lat_0, lon_0=lon_0)
-        # fill background.
-        m1.drawmapboundary(fill_color='aqua')
-        # draw coasts and fill continents.
-        m1.drawcoastlines(linewidth=0.5)
-        # m.fillcontinents(color='coral',lake_color='aqua')
-        # 20 degree graticule.
-        m1.drawparallels(np.arange(-80,81,20))
-        m1.drawmeridians(np.arange(-180,180,20))
-        m1.drawcountries(linewidth = 1)
-        m1.drawcoastlines(linewidth = 1)
-
-        m2 = Basemap(ax = ax[0,1],width=15.e6,height=15.e6,
-            projection='gnom',lat_0=60,lon_0=-30)
-        m2.drawmapboundary(fill_color='aqua')
-        m2.drawcoastlines()
-        # m.fillcontinents(color='coral',lake_color='aqua')
-        m2.drawparallels(np.arange(10,90,20))
-        m2.drawmeridians(np.arange(-180,180,30))
-        m2.drawcountries(linewidth = 1)
+        avail_stations = self.station_data[self.station_data.index.isin(stations)]        
+        self.cartopy_ax(fig, date_time=target_time, G=G, available_stations=avail_stations, proj='gnom', counts=counts, label = label)
+        # plt.savefig(f'plots/t_snap_{label}.png')
+        # plt.show()
 
 
-        m3 = Basemap(ax = ax[1,0],width=15.e6,height=15.e6,
-            projection='gnom',lat_0=-40.,lon_0=130)
-        m3.drawcoastlines()
-        m3.drawmapboundary(fill_color='aqua')
-        # m.fillcontinents(color='coral',lake_color='aqua')
-        m3.drawparallels(np.arange(10,90,20))
-        m3.drawmeridians(np.arange(-180,180,30))
-        m3.drawcountries(linewidth = 1)
+    def animation(self, label:str) ->None:
 
-
-        m4 = Basemap(ax = ax[1,1],width=15.e6,height=15.e6,
-            projection='gnom',lat_0=60,lon_0=120)
-        m4.drawcoastlines()
-        m4.drawmapboundary(fill_color='aqua')
-        # m.fillcontinents(color='coral',lake_color='aqua')
-        m4.drawparallels(np.arange(10,90,20))
-        m4.drawmeridians(np.arange(-180,180,30))
-        m4.drawcountries(linewidth = 1)
-
-
-        for ind1, ax in enumerate(ax.flat):
-            m = [m1,m2,m3,m4]
-            mx, my = m[ind1](available_stations['GEOLON'].values, available_stations['GEOLAT'].values)
-            pos = {}
-            print(ind1, ax)        
-            for ind2, station in enumerate(available_stations.index):
-                pos[station] = (mx[ind2], my[ind2])
-            if ind1 ==0:
-                freqs = [(1/600)*counts[station]**2.8 for station in G.nodes()]
-            else:
-                freqs = [0.01*counts[station]**2.8 for station in G.nodes()]
-            nx.draw_networkx_nodes(G = G, pos = pos, node_list = G.nodes(), 
-            node_color = 'r', alpha = 0.8, node_size = freqs, ax = ax)
-            nx.draw_networkx_edges(G = G, pos = pos, edge_color='g',
-            alpha=1, arrows = self.directed, ax = ax)
-
-        fig.suptitle(f'Pc2, Network snapshot at time {closest_time}')
-        plt.savefig('und_net_pc2_snapshot.png')
-
-        plt.show()
-
-
-        def net_animation(self) -> None:
-            # TODO: plot animation using t_snapshot for all times
-            pass
+        plt.rcParams['animation.ffmpeg_path'] = '/Users/sc/anaconda3/envs/geomag_env/bin/ffmpeg'
+        fig = plt.figure(figsize=(10,6))
+        camera = Camera(fig)
+        for n, t in enumerate(self.ordered_times[0:10]):
+            print(t,n,len(self.ordered_times))
+            self.t_snapshot(t,fig,label)
+            camera.snap()
+        animation = camera.animate()
+        animation.save('animation.mp4')
 
 # skipps bad lines
+# can show arrows, or look for conneced paths only can do a lot of analysis, need to find a way to show arrows
 
-# pd.set_option("display.max_rows", None, "display.max_columns", None)
-
-
+# data taken from http://wdc.kugi.kyoto-u.ac.jp/poles/polesexp.html 
+dict_geomag_poles = { 2000: {'north':   [79.6, -71.6] , 'south': [-79.6, 108.4]}, 
+2005: {'north':   [79.8, -71.8] , 'south': [-79.8, 108.2]},
+2006: {'north':   [79.8, -71.8] , 'south': [-79.8, 108.2]},
+2007: {'north':   [79.8, -71.8] , 'south': [-79.8, 108.2]},
+2008: {'north':   [79.8, -71.8] , 'south': [-79.8, 108.2]},
+2009: {'north':   [79.8, -71.8] , 'south': [-79.8, 108.2]},
+2010: {'north':   [80.1, -72.2] , 'south': [-80.1, 107.8]},
+2011: {'north':   [80.1, -72.2] , 'south': [-80.1, 107.8]},
+2012: {'north':   [80.1, -72.2] , 'south': [-80.1, 107.8]},
+2013: {'north':   [80.1, -72.2] , 'south': [-80.1, 107.8]},
+2014: {'north':   [80.1, -72.2] , 'south': [-80.1, 107.8]},
+2015: {'north':   [80.4, -72.6] , 'south': [-80.4, 107.4]},
+2016: {'north':   [80.4, -72.6] , 'south': [-80.4, 107.4]},
+2017: {'north':   [80.5, -72.6] , 'south': [-80.5, 107.4]},
+2018: {'north':   [80.5, -72.7] , 'south': [-80.5, 107.3]},
+2019: {'north':   [80.6, -72.7] , 'south': [-80.6, 107.3]},
+2020: {'north':   [80.7, -72.7] , 'south': [-80.7, 107.3]},
+2021: {'north':   [80.7, -72.7] , 'south': [-80.7, 107.3]},
+2022: {'north':   [80.7, -72.7] , 'south': [-80.7, 107.3]} }
 
 # print(pos)
 
 # need to think cheak weather all station coordinates data needed for each plot
 
-drawnet = DrawNetwork('networks_data/170313_all_available_stations_networks/na0_e_window_20_peakh_0.3_num_stations_70.txt', 70, directed=False)
-drawnet.t_snapshot('11:20:00')
+drawnet = DrawNetwork('networks_data/170313_all_available_stations_networks/dna0_e_window_20_peakh_0.3_num_stations_70.txt', 70, geomag_poles_coord=dict_geomag_poles, directed=False)
+# close_time = drawnet.t_close('11:20:00')
+# print(close_time)
+# drawnet.t_snapshot(close_time, fig = plt.figure(figsize=(15,10)))
+drawnet.animation('Directed network Pc2')
+# drawnet.net_animation()
 # print(dir(DrawNetwork))
 # print(drawnet.edge_list[0][2],type(drawnet.edge_list[0][2]))
 
@@ -170,7 +142,6 @@ drawnet.t_snapshot('11:20:00')
 # lat_ts is the latitude of true scale.
 # resolution = 'c' means use crude resolution coastlines.
 
-plt.show()
 # print(station_data['IAGA'],len(station_data['IAGA']))
 
 
--- PivotTable.py
+++ PivotTable.py
@@ -1,46 +1,39 @@
-import pandas as pd
-import pickle
-import networkx as nx
-import ast
 import numpy as np
-
+import pandas as pd
+import networkx as nx
+from collections import Counter
 
 class PivotTable:
     def __init__(self, edge_list_dir: str, n: int):
-        G = nx.read_edgelist('test_e_170313_15_0.3.txt')
+        G = nx.read_edgelist(edge_list_dir)
         self.edge_list = list(G.edges(data=True))
         self.n = n
+        # initializing function runs create_PivotTable() function  creats self.pivot table object
+        self.create_PivotTable()
         
     def create_PivotTable(self) -> None:
-        curr_index = 0
-        curr_time = edge_list[curr_index][2]['attr_dict']['UTC1']
-        
+
+        node_counter_dict = dict()
+        for edge in self.edge_list:
+
+            edge_time = edge[2]['attr_dict']['UTC1']
+            if edge_time not in node_counter_dict:
+                node_counter_dict[edge_time] = Counter()
+            node_counter_dict[edge_time][edge[0]] += 1
+            node_counter_dict[edge_time][edge[1]] += 1
+            
+          
         self.pivot_table = pd.DataFrame([])
-        
-        while curr_index < len(edge_list):
-            node_counter = Counter()
-            while curr_index < len(edge_list) and edge_list[curr_index][2]['attr_dict']['UTC1'] == curr_time:
-                curr_edge = edge_list[curr_index]
-                node_counter[curr_edge[0]] += 1
-                node_counter[curr_edge[1]] += 1
-                curr_index +=1
-            
-            if curr_index >= len(edge_list):
-                break
-                
-                
-            new_row = pd.DataFrame(index=[curr_time],columns=[np.arange(1,self.n)])
+        for time, node_counter in node_counter_dict.items():
+            # print(time,node_counter)
+            new_row = pd.DataFrame(index=[time], columns=[np.arange(1,self.n)])
             self.pivot_table = self.pivot_table.append(new_row)
-            
+
             degree_counts = Counter(list(node_counter.values()))
+            # print(degree_counts)
             for degree_key, degree_count in degree_counts.items():
-                self.pivot_table.loc[curr_time, degree_key] = degree_count
+                # print(degree_key, degree_count)
+                self.pivot_table.loc[time, degree_key] = degree_count
+
                 
-            curr_time = edge_list[curr_index][2]['attr_dict']['UTC1']
-            
-    def plot_pivot_table(self) -> None:
-        # TODO: PLot Heatmap
-        pass
-
-
-
+        self.pivot_table = self.pivot_table.sort_index()
