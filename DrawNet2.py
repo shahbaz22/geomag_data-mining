2c1e37843d51beba6ddc0981840ca18ae020f569
shahbaz22 <5h4h842.c@gmail.com>
2022-07-28 12:02:22 +0100

    added overdue changes to plotting files

---
+++ DrawNet2.py
@@ -0,0 +1,295 @@
+import numpy as np
+import numpy as np
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+from cartopy.feature.nightshade import Nightshade
+import cartopy.feature as cfeature
+from datetime import datetime
+from collections import Counter
+import matplotlib.gridspec as gridspec
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+import os
+from os import listdir
+from os.path import isfile, join
+
+# station_data = pd.read_csv('supermag-stations.csv')
+# station_data.set_index('IAGA', inplace=True)
+# mag_coords_dict1 = {}
+# mag_coords_dict2 = {}
+# mag_coords_dict_diff = {}
+# A = Apex(date=2015.3)
+# for station , v in station_data.to_dict('index').items():
+#     mlat, mlon = A.convert(v['GEOLAT'], v['GEOLON'], 'geo', 'apex', height=0)
+#     mag_coords_dict1[station] = (mlon, mlat)
+
+# for station , v in station_data.to_dict('index').items():
+#     mlat, mlon = v['AACGMLAT'], v['AACGMLON']
+#     mag_coords_dict2[station] = (mlon, mlat)
+
+# for station , v in station_data.to_dict('index').items():
+#     coords1 = mag_coords_dict1[station]
+#     coords2 = mag_coords_dict2[station]
+#     mag_coords_dict_diff[station]= (abs(coords1[0])-abs(coords2[0]), abs(coords1[1])-abs(coords2[1]))
+
+# diff_list = mag_coords_dict_diff.values()
+# vals = max(diff_list,key=lambda item:item[1])
+# print(vals)
+# print(list(mag_coords_dict_diff.keys())[list(diff_list).index(vals)])
+# print(mag_coords_dict1['TAM'])
+
+# # rework with cartopy
+# # first want to draw network for single specific time, eventually want to run this 
+# # plotting class a bunch of times (class usful to acces attributes and run methods)
+# # Need to creat a dictonary which maps station name to (key)conda evs
+
+
+class DrawNetwork:
+    def __init__(self, edge_list_dir: str, n: int, directed: bool, geomag_poles_coord:dict, cluster:bool, conn_cluster:list):
+        G = nx.read_edgelist(edge_list_dir)
+        self.edge_list = list(G.edges(data=True))
+        all_times = [s[2]['attr_dict']['UTC'] for s in self.edge_list]
+        self.ordered_times = sorted(list(set(all_times)))
+        self.n = n
+        self.directed = directed
+        self.station_data = pd.read_csv('supermag-stations.csv')
+        self.station_data.set_index('IAGA', inplace=True)
+        self.geomag_poles_coord = geomag_poles_coord
+        self.date = self.edge_list[0][2]['attr_dict']['UTC'].split(' ')[0]
+        self.fig = plt.figure(figsize=(13,8))
+        self.gs = self.fig.add_gridspec(2, 3)
+        self.cluster = cluster
+        self.conn_cluster = conn_cluster
+        # self.apex_coords()
+
+    def apex_coords(self):
+        year = int(self.date.split('-')[0])
+        month = int(self.date.split('-')[1])
+        self.mag_coords_dict1 = {}
+        A = Apex(date=f'{year}.{month}')
+        for station , v in self.station_data.to_dict('index').items():
+            mlat, mlon = A.convert(v['GEOLAT'], v['GEOLON'], 'geo', 'apex', height=0)
+            self.mag_coords_dict1[station] = (mlon, mlat)
+        return self.mag_coords_dict1
+
+    def t_close(self, target_time: str) -> str:
+        date_time = f'{self.date} {target_time}'
+        date_format = '%Y-%m-%d %H:%M:%S'
+        self.closest_time = min(self.ordered_times, key=lambda x: 
+            abs(datetime.strptime(x, date_format) - datetime.strptime(date_time, date_format)))
+        self.datetime_closest_time = datetime.strptime(self.closest_time,'%Y-%m-%d %H:%M:%S')
+        return self.closest_time
+
+    def color_map_pc(self, pc_powers:list)->list:
+        sorted_pc_powers = sorted(pc_powers)
+        print(sorted_pc_powers)
+        norm = mpl.colors.Normalize(vmin=min(sorted_pc_powers), vmax=max(sorted_pc_powers), clip=True)
+        mapper = mpl.cm.ScalarMappable(norm=norm, cmap=mpl.cm.coolwarm)
+        c_transform = [mapper.to_rgba(i) for i in pc_powers]
+        return c_transform
+
+
+    def cartopy_ax(self, gridspec: plt.figure, date_time: str, G:nx.Graph, available_stations:list, proj: str, counts:dict, label:str, geographic:bool) ->None:
+
+        year = self.date.split('-')[0]
+        gn_lat, gn_lon  = self.geomag_poles_coord[int(year)]['north']
+        gs_lat, gs_lon = self.geomag_poles_coord[int(year)]['south']
+        dt = datetime.strptime(date_time,'%Y-%m-%d %H:%M:%S')
+        source_proj = ccrs.PlateCarree() 
+        if geographic == True:
+            if proj == 'globe':
+                map_proj = ccrs.PlateCarree(central_longitude=gn_lon)
+                ax = self.fig.add_subplot(gridspec, projection = map_proj)
+                title = ax.text(0.5,1.05, "", bbox={'facecolor':'w', 'alpha':0.5, 'pad':5}, transform=ax.transAxes, ha="center")
+                title.set_text(f'{label}\n{date_time} UTC')
+            elif proj == 'south_pole':
+                map_proj = ccrs.Orthographic(central_longitude=gs_lon, central_latitude=gs_lat)
+                ax = self.fig.add_subplot(gridspec, projection = map_proj)
+            elif proj == 'north_pole':
+                map_proj = ccrs.Orthographic(central_longitude=gn_lon, central_latitude=gn_lat)
+                ax = self.fig.add_subplot(gridspec, projection = map_proj)
+            ax.stock_img()
+            ax.add_feature(cfeature.GSHHSFeature(edgecolor='k'))
+            ax.add_feature(Nightshade(dt, alpha=0.4))
+            ax.gridlines(source_proj, xlocs=np.linspace(-180,180,11), linestyle='--', draw_labels=False)
+            ax.scatter(x=[gn_lon,gs_lon], y=[gn_lat,gs_lat], color="orange", s=100,alpha=1, transform=source_proj)
+            pos = {}
+            pos = {station: (map_proj.transform_point(v['GEOLON'], v['GEOLAT'], src_crs=source_proj))
+               for station, v in
+               available_stations.to_dict('index').items()}
+        elif proj == 'geomag':
+            map_proj = ccrs.PlateCarree() 
+            ax = self.fig.add_subplot(gridspec, projection = map_proj)
+            title = ax.text(0.5,1.2, "", bbox={'facecolor':'w', 'alpha':0.5, 'pad':5}, transform=ax.transAxes, ha="center")
+            title.set_text(f'Geomagnetic coordinates')
+            ax.set_extent([-180, 180, -90, 90], source_proj)
+            ax.gridlines(source_proj, linestyle='--', draw_labels=True)
+            ax.set_xticks([-180, -120, -60, 0, 60, 120, 180], crs=map_proj)
+            ax.set_yticks([-78.5, -60, -25.5, 25.5, 60, 80], crs=map_proj)
+            ax.xformatter = LONGITUDE_FORMATTER
+            ax.yformatter = LATITUDE_FORMATTER
+
+            # pos = {s: self.mag_coords_dict1[s] for s in available_stations.to_dict('index')}
+            pos = {}
+            pos = {station: (map_proj.transform_point(v['AACGMLON'], v['AACGMLAT'], src_crs=source_proj))
+               for station, v in
+               available_stations.to_dict('index').items()}
+
+        # str split here to obtain pc_power
+        freqs = [counts[station]**2 for station in G.nodes()]
+        freqs = np.array(freqs)*0.01/8
+        nx.draw_networkx_nodes(G = G, pos = pos, nodelist = G.nodes(), node_color='blue', alpha = 0.8, node_size=10, ax = ax)
+        # nx.draw_networkx_edges(G = G, pos = pos, edge_color='g',
+        # alpha=1, arrows = self.directed, ax = ax, nodelist = G.nodes())
+
+        if self.cluster==True:
+            # reason for nodes in cluster not beeing connected to each other because they may be connected to another cluster in another group
+            c_nodes = [station for station, conn in counts.items() if conn <= self.conn_cluster[1] and conn >= self.conn_cluster[0]]
+            c_freqs = [(counts[station]**2)*0.01 for station in c_nodes]
+            sub_G = G.subgraph(c_nodes)
+            nx.draw_networkx_nodes(G = sub_G, pos = pos, nodelist = sub_G.nodes(), node_color='red', alpha = 0.8, node_size = 10, ax = ax)
+            nx.draw_networkx_edges(G = sub_G, pos = pos, edge_color='lime',
+            alpha=1, arrows = self.directed, ax = ax, nodelist = sub_G.nodes())
+
+
+    def t_snapshot(self, target_time: str, label:str, index:int, save=False) -> None:
+        
+        t_edge_list = []
+        for line in self.edge_list:
+            if line[2]['attr_dict']['UTC'] == target_time:
+                t_edge_list.append([line[0].split('_')[0], line[1].split('_')[0]])
+
+        if self.directed == True:
+            G = nx.DiGraph(t_edge_list)
+        else:
+            G = nx.Graph(t_edge_list)
+
+        reshaped_edge_list = np.reshape(t_edge_list, len(t_edge_list)*2)
+        stations = list(set(reshaped_edge_list))
+        # print(stations)
+
+        counts = Counter(reshaped_edge_list)
+        avail_stations = self.station_data[self.station_data.index.isin(stations)]        
+        self.cartopy_ax(self.gs[0,1], date_time=target_time, G=G, available_stations=avail_stations, 
+            counts=counts, label = label, proj='globe', geographic=True)
+        self.cartopy_ax(self.gs[1,:], date_time=target_time, G=G, available_stations=avail_stations, 
+            counts=counts, label = label, proj='geomag', geographic=False)
+        self.cartopy_ax(self.gs[0,0], date_time=target_time, G=G, available_stations=avail_stations, 
+            counts=counts, label = label, proj='south_pole', geographic=True)
+        self.cartopy_ax(self.gs[0,2], date_time=target_time, G=G, available_stations=avail_stations, 
+            counts=counts, label = label, proj='north_pole', geographic=True) 
+
+        if save==True:     
+            plt.savefig(f'plots/movie_plots/t_snap_{label}_{index}.png')
+        # plt.show()
+        plt.clf()
+
+
+    def split_animation(self, chunks:int, label:str, event:str) ->None:
+        'for large animations, use frames less than 200'
+
+        all_times = np.array(self.ordered_times)
+        times_arr = np.array_split(all_times, chunks)
+        for ind, times in enumerate(times_arr):
+            print('chunk',ind)
+            self.animation(label, f'{event}_section_{ind}', time=times)
+    
+    def animation1(self, label:str, event:str, time:list) ->None:
+
+        # plt.rcParams['animation.ffmpeg_path'] = '/Users/sc/anaconda3/envs/geomag_env/bin/ffmpeg'
+        # plt.rcParams['animation.ffmpeg_path'] = '~/anaconda3/envs/geomag/bin/ffmpeg'
+        camera = Camera(self.fig)
+        for n, t in enumerate(time):
+            print(t,n,len(time))
+            self.t_snapshot(t, label,index=None)
+            camera.snap()
+        animation = camera.animate()
+        animation.save(f'gifs/{label}_{event}.gif', fps=1)
+    
+    def animation2(self, label:str, event:str, time:list) ->None:
+
+        for n, t in enumerate(time):
+            print(t,n,len(time))
+            self.t_snapshot(t, label,index=n, save=True)
+        
+        # home settings
+        # save_file_path = '/Users/sc/conda_envs/geomag/plots/movie_plots/'
+        # ffmpeg_path = '/Users/sc/anaconda3/envs/geomag_env/bin/ffmpeg'
+        # remote settings
+        save_file_path = '/home/space/phrmfl/Shared/disk/_Users_sc_conda_envs_geomag/plots/movie_plots/'
+        ffmpeg_path = '~/.conda/envs/geomag/bin/ffmpeg'        
+
+        os.system('{} -r 4 -s 2000x2000 -f image2 -i \
+            plots/movie_plots/t_snap_{}_%01d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p \
+            {}{}_{}.mp4'.format(ffmpeg_path,label, save_file_path, label, event))
+        os.system('rm {}*.png'.format(save_file_path))
+
+
+
+# data taken from http://wdc.kugi.kyoto-u.ac.jp/poles/polesexp.html 
+dict_geomag_poles = { 2000: {'north':   [79.6, -71.6] , 'south': [-79.6, 108.4]}, 
+2005: {'north':   [79.8, -71.8] , 'south': [-79.8, 108.2]},
+2006: {'north':   [79.8, -71.8] , 'south': [-79.8, 108.2]},
+2007: {'north':   [79.8, -71.8] , 'south': [-79.8, 108.2]},
+2008: {'north':   [79.8, -71.8] , 'south': [-79.8, 108.2]},
+2009: {'north':   [79.8, -71.8] , 'south': [-79.8, 108.2]},
+2010: {'north':   [80.1, -72.2] , 'south': [-80.1, 107.8]},
+2011: {'north':   [80.1, -72.2] , 'south': [-80.1, 107.8]},
+2012: {'north':   [80.1, -72.2] , 'south': [-80.1, 107.8]},
+2013: {'north':   [80.1, -72.2] , 'south': [-80.1, 107.8]},
+2014: {'north':   [80.1, -72.2] , 'south': [-80.1, 107.8]},
+2015: {'north':   [80.4, -72.6] , 'south': [-80.4, 107.4]},
+2016: {'north':   [80.4, -72.6] , 'south': [-80.4, 107.4]},
+2017: {'north':   [80.5, -72.6] , 'south': [-80.5, 107.4]},
+2018: {'north':   [80.5, -72.7] , 'south': [-80.5, 107.3]},
+2019: {'north':   [80.6, -72.7] , 'south': [-80.6, 107.3]},
+2020: {'north':   [80.7, -72.7] , 'south': [-80.7, 107.3]},
+2021: {'north':   [80.7, -72.7] , 'south': [-80.7, 107.3]},
+2022: {'north':   [80.7, -72.7] , 'south': [-80.7, 107.3]} }
+
+file_path = 'networks_data/2015'
+station_names = [f.split('_')[1] for f in listdir(file_path) if isfile(join(file_path, f))]
+# print('Enter event year, 2012, 2013, 2015:')
+# year = input()
+# print('Enter event component:')
+# comp = input()
+comp ='n'
+year='2015'
+stations = 128
+fhandel = f'{stations}_0.3_0.25_2.5_{year}.txt'
+path = f'networks_data/{year}_nets/comp_{comp}/'
+# print(station_names, len(station_names))
+t= '6:55:00'
+conn_rangest1 = [[10,20],[80,105]]
+conn_rangest2  = [[10,30]]
+times = ['6:25:00', '7:45:00','9:30:00']
+
+
+for conn_range in conn_rangest2:
+    t=times[2]
+    for i in [0]:    
+        for net_type in ['undir_net_antiphase','undir_net_inphase','dir_net_t1']:
+                if i ==1:
+                    conn_range = [30,50]
+                    drawnet = DrawNetwork(f'{path}combined_net{i}.txt',
+                     128, geomag_poles_coord=dict_geomag_poles, directed=False, cluster=True, conn_cluster=conn_range)
+                    label = f'combined_net_pc{i+2}_{t}_{comp}_num_conn_range_{conn_range[0]}-{conn_range[1]}'
+                    print(net_type, comp, year, f'Pc{i+2}')
+                    close_time = drawnet.t_close(t)
+                    # label used for saving and title name
+                    drawnet.t_snapshot(close_time, label=label, index=1, save=True) 
+                    break
+                else:
+                    drawnet = DrawNetwork(f'{path}{net_type}{i}_{comp}_{fhandel}',
+                     128, geomag_poles_coord=dict_geomag_poles, directed=False,  cluster=True, conn_cluster=conn_range)
+                    label = f'{net_type}_Pc{i+2}_{t}_{comp}_num_conn_range_{conn_range[0]}-{conn_range[1]}'
+                    print(net_type, comp, year, f'Pc{i+2}')
+                    close_time = drawnet.t_close(t)
+                    drawnet.t_snapshot(close_time, label=label, index=1, save=True) 
+
+              
+                # drawnet.animation2(label= f'{net_type}_Pc{i+2}_{comp}_{year}',event=f'{net_type}_0.4', time=drawnet.ordered_times)
+
+
+
--- PlotNet2.py
+++ PlotNet2.py
@@ -19,7 +19,9 @@ import matplotlib.gridspec as gridspec
 from matplotlib import rc
 from tqdm import tqdm
 import sys
+from matplotlib.colors import LogNorm
 import os
+
 class PlotNet: 
     def __init__(self, net_labs:list, ulf_fname:str, n:int, dt_start:str, dt_end:str):
         'comp needs to be input array and also bands'
@@ -31,10 +33,11 @@ class PlotNet:
         # fname always ends in date and not .txt and includes path
         self.year = dt_start.split(' ')[0].split('-')[0]
         # create save path and save_path fname based on inputs
+
     # can ajust file hadnels and paths from this function
     def path_and_fhandel(self, comp, band):
-        path = f'networks_data/{self.year}_nets/comp_{comp}/smr_min/'
-        fhandel = f'{band}_{comp}_{self.n}_0.3_0.25_2.5_{self.year}smr_min.txt'
+        path = f'networks_data/{self.year}_nets/comp_{comp}/'
+        fhandel = f'{band}_{comp}_{self.n}_0.3_0.25_2.5_{self.year}.txt'
         return path, fhandel
 
     def utc_sort_reduce(self, utc_times, deliminator='T'):
@@ -61,21 +64,51 @@ class PlotNet:
         also ulf power from edgelist network files'''
         mlt_lower_lim = 9
         path, fhandel = self.path_and_fhandel(comp, band)
+
         if manual_label==False:
             fname = path + net_label + fhandel
         else: 
             fname = f'{path}{net_label}'
-        netdistr = NetDistr(fname, self.n)
+        
+        try:
+            netdistr = NetDistr(fname, self.n)
+        except FileNotFoundError:
+            print(f'File {fname} does not exist, change in self.path_and_fhandel')
+        
         if netdistr.num_edges<2:
             print('pivot table empty')
             exit()
         pt = netdistr.create_pivottable(self.dt_start, self.dt_end, f'pc{band+2}')
         pt.index = pd.to_datetime(pt.index)
         pt_mlt = netdistr.create_pivottable_by_mlt(mlt_lower_lim, self.dt_start, self.dt_end, f'pc{band+2}')
-        pt.index = pd.to_datetime(pt.index)
         print(net_label, band, comp)
         return pt, pt_mlt
 
+    def plotting_files_lags(self, net_label:str, band:int, comp:str, pm:int, mlt:bool, manual_label=False):
+        '''Func to create all plotting data including pivot talbe for undirected and directed networks
+        also ulf power from edgelist network files'''
+        path, fhandel = self.path_and_fhandel(comp, band)
+        if manual_label==False:
+            fname = path + net_label + fhandel
+        else: 
+            fname = f'{path}{net_label}'
+        
+        try:
+            netdistr = NetDistr(fname, self.n)
+        except FileNotFoundError:
+            print(f'File {fname} does not exist, change in self.path_and_fhandel')
+            exit()
+
+        if netdistr.num_edges<2:
+            ('pivot table empty')
+            exit()
+        if mlt==True:
+            pt = netdistr.create_pivottable_lags_mlt(9, self.dt_start, self.dt_end, f'pc{band+2}', period_mult=pm)
+        else:
+            pt = netdistr.create_pivottable_lags(self.dt_start, self.dt_end, f'pc{band+2}', period_mult=pm)
+        pt.index = pd.to_datetime(pt.index)
+        print(net_label, band, comp)
+        return pt
 
     def combine_files(self, fnames:list, path:str, band:int):
         with open(f'{path}combined_net{band}.txt', 'w') as outfile:
@@ -311,35 +344,113 @@ class PlotNet:
             plt.savefig(f'plots/{year}_nets/comp_{comp}/nets_{save_label}_dist_ts.png', facecolor='w')
 
         # plt.show()
-for i in [0]:
-    if i ==0:
-        year = '2015'
-        num_stations = 128
-        # dt_start = '2015-03-17 4:00:00'
-        # dt_end = '2015-03-17 12:00:00'
-        dt_start ='2015-03-17 19:30:00'
-        dt_end='2015-03-18 03:30:00'
-        ulf_fname = 'networks_data/ulf_spd_2015.csv'
-        indices_fname = 'networks_data/indices_spd_2015.csv'
-    if i ==1:
-        num_stations = 133
-        dt_start = '2012-01-21 20:00:00'
-        dt_end = '2012-01-22 16:00:00'
-        ulf_fname = 'networks_data/2012-01-21_22-ulf.csv'
-        indices_fname = 'networks_data/2012-01-21_22_indices.csv'
-    if i ==2:
-        num_stations = 122
-        dt_start = '2013-03-17 4:00:00'
-        dt_end = '2013-03-17 12:00:00'
-        ulf_fname = 'networks_data/17march2013_4am.csv'
-        indices_fname = 'networks_data/170313_indices.csv'
 
-    # hacky solution to include all these netwokr and combined Pc3 network
-    net_labs = ['dir_net_t1', 'undir_net_inphase', 'undir_net_antiphase']
-    # pc2 t1, in_phase, anti_phase, pc3 combined
 
-    pn = PlotNet(net_labs, ulf_fname, num_stations, dt_start, dt_end)
-    for comp in ['n','e','z']:
-        lab = '_'.join(net_labs)
-        pn.plot_dist_ts(indices_fname, net_labs, comp, y_space=10, save_plot=True, save_label=f'{lab}_smr_min')     
+    def plot_dist_ts_lags(self, indices_fname:str, comp:str, pm:int, save_label:str, save_plot:bool):
+        plt.rcParams.update({'font.size': 18})
+        num_plots = 3
+        fig = plt.figure(figsize=(20, 17), constrained_layout=True)
+        gs = fig.add_gridspec(num_plots, 3, width_ratios=[30,1,1])
+        y_interval=[10,26]
+        ylims = [[40,-40],[130,-130]]
+        colour1 = 'Purples_r'
+        for i in [0,1]:
+            heat_ax1, colorbar_ax1 = fig.add_subplot(gs[i,0]), fig.add_subplot(gs[i,1])    
+            label = 'combined_net'
+            pt1 = self.plotting_files_lags(f'combined_net{i}.txt', i, comp, pm, mlt=False, manual_label=True)
+
+            # ------ sorting by setting range of lags and limits
+            # pt1 = pt1.reindex(sorted(pt1.columns), axis=1)
+            pt1 = pt1[np.linspace(ylims[i][1],ylims[i][0],2*ylims[i][0]+1)]
+            # # ---------
+            ax1 = sb.heatmap(pt1.T, xticklabels = 3, yticklabels = y_interval[i], ax = heat_ax1, cmap = colour1, 
+            cbar_kws={'label': 'frequency'}, cbar_ax = colorbar_ax1, norm=LogNorm())
+            ax1.axes.get_xaxis().set_visible(False)
+            heat_ax1.grid()
+            heat_ax1.text(0.1, 1, f'{label}_pc{i+2}_{comp}', transform= heat_ax1.transAxes, bbox = dict(facecolor = 'white', alpha = 0.5))
+            heat_ax1.invert_yaxis()
+            heat_ax1.set_ylabel('lag')
+            heat_ax1.set_facecolor('grey')
+            heat_ax1.set_facecolor('xkcd:black')
+
+
+        indices = pd.read_csv(indices_fname)
+        index_date = indices['Date_UTC']
+        sme = indices['SME']
+        axsmer = pd.to_datetime(index_date)
+        datemin = datetime.strptime(self.dt_start, '%Y-%m-%d %H:%M:%S')
+        datemax = datetime.strptime(self.dt_end, '%Y-%m-%d %H:%M:%S')
+        f1 = fig.add_subplot(gs[2,0])
+        f1.plot(axsmer,sme, color='black', label = 'SME')
+        f1.set_ylabel('SME (nT)')
+        f1.grid()
+        f1.set_xlim(datemin,datemax)
+        formatter = mdates.DateFormatter("%H:%M:%S")
+        f1.xaxis.set_major_formatter(formatter)
+        f1.legend()
+        f1.set_xlim(datemin,datemax)
+        ax2 = f1.twinx()
+        
+        # code for plotting SMR sections for entire SMR yearly dataset
+        indices['Date_UTC'] = pd.to_datetime(indices['Date_UTC']) 
+        for i in ['SMR00','SMR06','SMR12','SMR18' ]:
+            x = axsmer[(indices['Date_UTC'] >= datemin) & (indices['Date_UTC'] <= datemax)]
+            y = indices[(indices['Date_UTC'] >= datemin) & (indices['Date_UTC'] <= datemax)][i]
+            ax2.plot(x , y, label= i)
+        ax2.xaxis.set_major_formatter(formatter)
+        ax2.legend(loc='upper center',ncol=4, bbox_to_anchor=[0.5, 1.0])
+        ax2.set_ylabel('SMR (nT)')
+        ax2.grid()
+        ax2.set_xlim(datemin,datemax)
+
+        
+        if save_plot:
+            year = self.dt_end.split('_')[0].split('-')[0]
+            s_path = f'/Users/sc/conda_envs/geomag/plots/{year}_nets/comp_{comp}/'
+            path_exists = os.path.exists(s_path)
+            if not path_exists:
+                # Create a new directory because it does not exist 
+                os.makedirs(s_path)
+                print(f"The new directory {s_path} is created!")
+            plt.savefig(f'plots/{year}_nets/comp_{comp}/nets_{save_label}_dist_lags_ts.png', facecolor='w')
+        plt.show()
+
+dn = NetDistr('networks_data/2015_nets/comp_e/combined_net0.txt',128)
+df = dn.ts_conj_conn()
+df.plot()
+plt.show()
+
+# for i in [0]:
+#     if i ==0:
+#         year = '2015'
+#         num_stations = 128
+#         dt_start = '2015-03-17 4:00:00'
+#         dt_end = '2015-03-17 12:00:00'
+#         # dt_start ='2015-03-17 19:30:00'
+#         # dt_end='2015-03-18 03:30:00'
+#         ulf_fname = 'networks_data/ulf_spd_2015.csv'
+#         indices_fname = 'networks_data/indices_spd_2015.csv'
+#     if i ==1:
+#         num_stations = 133
+#         dt_start = '2012-01-21 20:00:00'
+#         dt_end = '2012-01-22 16:00:00'
+#         ulf_fname = 'networks_data/2012-01-21_22-ulf.csv'
+#         indices_fname = 'networks_data/2012-01-21_22_indices.csv'
+#     if i ==2:
+#         num_stations = 122
+#         dt_start = '2013-03-17 4:00:00'
+#         dt_end = '2013-03-17 12:00:00'
+#         ulf_fname = 'networks_data/17march2013_4am.csv'
+#         indices_fname = 'networks_data/170313_indices.csv'
+
+#     # hacky solution to include all these netwokr and combined Pc3 network
+#     # net_labs = ['dir_net_t1', 'dir_net_tn', 'undir_net_inphase', 'undir_net_antiphase']
+#     net_labs = ['dir_net_t1']
+#     # pc2 t1, in_phase, anti_phase, pc3 combined
+
+#     pn = PlotNet(net_labs, ulf_fname, num_stations, dt_start, dt_end)
+#     for comp in ['n','e','z']:
+#         lab = '_'.join(net_labs)
+#         # pn.plot_dist_ts_lags(indices_fname, net_labs, comp, y_space=10, save_plot=False, save_label=f'{lab}_smr_min')
+#         pn.plot_dist_ts_lags(indices_fname, comp , pm=2, save_plot=True, save_label=f'{lab}')     
 
